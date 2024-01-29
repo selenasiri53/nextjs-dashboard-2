5d784193c92fa1ff6261eb051f7d043e7d6ef41b
selenasiri53 <selenasiri53@gmail.com>
2024-01-29 09:30:55 -0500

    layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
+
+// Partial rendering - the layout won't rerender, only the component will load
+// In Nextjs the layout has children props - in this case its the dashboard page, customers' and invoices' pages
+// that will be nested inside
+
+// contains the root layout
