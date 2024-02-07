a28ae33f913e7ce2ce816b9fb3861fe5b93864ae
selenasiri53 <selenasiri53@gmail.com>
2024-02-07 08:31:09 -0500

    streaming revenueChart

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -5,3 +5,6 @@ export default function Loading() {
 }
 
 // Since loading.tsx is a higher level than customers/page.tsx and invoices/page.tsx, its applied to those pages too
+
+// loading.tsx will apply to all pages below it: /invoices/page.tsx & /customers/page.tsx
+// most commonly used as a placeholder/fallback for companies
---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+import DashboardSkeleton from "../ui/skeletons"; // new 
+
+export default function Loading() {
+    return <DashboardSkeleton />
+  }
+
+// Any UI added to loading.tsx will be embedded as part of the static file
