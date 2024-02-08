56332e4c81ca743ca123088bb28ebeabbab7d6ef
selenasiri53 <selenasiri53@gmail.com>
2024-02-07 21:32:36 -0500

    pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,14 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data'; // delete fetchRevenue, fetchLatestInvoices
-import { Suspense } from 'react'; // new
-import { RevenueChartSkeleton,  LatestInvoicesSkeleton, CardsSkeleton, } from '@/app/ui/skeletons'; // new
+import { fetchCardData } from '@/app/lib/data'; 
+import { Suspense } from 'react'; 
+import { RevenueChartSkeleton,  LatestInvoicesSkeleton, CardsSkeleton, } from '@/app/ui/skeletons'; 
 import CardWrapper from '@/app/ui/dashboard/cards';
  
 export default async function Page() {
-    // const revenue = await fetchRevenue();
-    // const latestInvoices = await fetchLatestInvoices();
+
     const {
         numberOfInvoices,
         numberOfCustomers,
@@ -22,7 +21,6 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* new - cards fetched in /ui/dashboard/cards.tsx */}
       <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
@@ -39,11 +37,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}> 
           <RevenueChart />
         </Suspense>
-        {/* new */}
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
         </Suspense>
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
   );
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,17 +2,18 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce';
+import { useDebouncedCallback } from 'use-debounce'; 
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
   
-  const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`); // optimizing the search
+  const handleSearch = useDebouncedCallback((term) => { 
+    console.log(`Searching... ${term}`); 
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1'); // new - set the initial page
     if (term) {
       params.set('query', term);
     } else {
